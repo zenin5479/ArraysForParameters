75a72baa33afb06d11d77ef5c3b332c8e41f9740
zenin5479 <zenin5479@gmail.com>
2025-09-27 14:27:28 +0300

    Update 0.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace ArraysForParameters
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
 }

127018cbea21343f378ddc65e9aa8a429bf41d2a
zenin5479 <zenin5479@gmail.com>
2025-09-27 16:00:27 +0300

    Update 0.2 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -9,4 +9,37 @@ namespace ArraysForParameters
          Console.WriteLine("Hello World!");
       }
    }
+
+   //setlocale(LC_ALL, "Russian");
+   //int n;
+   //printf("Введите размер векторов\n");
+   //scanf("%i", &n);
+   //float* C;
+   //float* P;
+   //printf("Введите вектор С\n");
+   //VvodVect(C, n);
+   //printf("Введите вектор P\n");
+   //VvodVect(P, n);
+   //printf("Введен вектор С\n");
+   //VivodVect(C, n);
+   //printf("Введен вектор P\n");
+   //VivodVect(P, n);
+   //float s = 0.0;
+
+   //s = Sum(C, n);
+   //if (Check(P, n, s))
+   //{
+   //   printf("Условие выполнено\n");
+   //   float** A;
+   //   FormMatrix(A, C, P, n);
+   //   VivodMatrix(A, n);
+   //   int i, j;
+   //   FindMax(A, n, i, j);
+   //   printf("Индексы максимума (%i,%i)", i, j);
+   //}
+
+   //else
+   //{
+   //   printf("Условие не выполнено");
+   //}
 }

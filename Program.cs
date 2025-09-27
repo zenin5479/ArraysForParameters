12a0082808717715b144ad008448dfddbfdf9c16
zenin5479 <zenin5479@gmail.com>
2025-09-27 14:25:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArraysForParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
